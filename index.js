0399668cfdf9f6168c5f6c5a0adb633e53a93006
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2021-06-23 23:47:59 +0600

    revision

--- index.js
+++ index.js
@@ -3,57 +3,84 @@
 // Author: Mohiuddin Mazumder
 // Date: 19/06/2021
 
-// //Dependencies
-// const http = require("http");
-// const url = require("url");
-// const { StringDecoder } = require("string_decoder");
-// const {handleReqRes} = require("./helpers/handleReqRes");
+//Dependencies
+const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
+const {handleReqRes} = require("./helpers/handleReqRes");
 
-// //app object = module scaffolding
-// const app = {};
-
-// //configuration
-// app.config = {
-//   port: 5000,
-// };
-
-// //handle request response
-// app.handleReqRes = handleReqRes;
-
-// //create server
-// app.createServer = () => {
-//   const server = http.createServer(app.handleReqRes);
-//   server.listen(app.config.port, () => {
-//     console.log(`listening to port ${app.config.port}`);
-//   });
-// };
-
-
-// //start the server
-// app.createServer();
-
-//dependencies.
-const http = require('http');
-
-//app object- module scaffolding
+//app object = module scaffolding
 const app = {};
 
-// configuration object
+//configuration
 app.config = {
   port: 5000,
-}
+};
+
+//handle request response
+app.handleReqRes = handleReqRes;
 
 //create server
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
   server.listen(app.config.port, () => {
-    console.log(`listening on port ${app.config.port}`);
+    console.log(`listening to port ${app.config.port}`);
   });
 };
 
-//handle Requests response
-app.handleReqRes = (req, res) => {
-  res.end('Hi i am running.')
-}
 
+//start the server
 app.createServer();
+
+// //dependencies.
+// const http = require('http');
+// const url = require('url');
+// const { StringDecoder} = require("string_decoder");
+
+
+// //app object- module scaffolding
+// const app = {};
+
+// // configuration object
+// app.config = {
+//   port: 5000,
+// }
+
+// //handle Requests response
+// app.handleReqRes = (req, res) => {
+//    //request handling
+//    //get the url and parse it
+//    const parsedUrl = url.parse(req.url, true); //true means consider the query string 
+//    const path = parsedUrl.path;
+//    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+//    const method = req.method.toLowerCase(); //req.method is used for what kind of request coming form client site
+//    const queryStringObject = parsedUrl.query;
+//    const headersObject = req.headers;
+  
+//    const decoder = new StringDecoder('utf-8');
+//    let realData = "";
+
+//    req.on('data', (buffer) =>{
+//          realData += decoder.write(buffer);
+//    })
+
+//    req.on('end', () =>{
+//      realData += decoder.end();
+//      console.log(realData);
+//      //response handling
+//     res.end('Hi i am running.')
+//    })
+  
+// }
+
+// //create server
+// app.createServer = () => {
+//   const server = http.createServer(app.handleReqRes);
+//   server.listen(app.config.port, () => {
+//     console.log(`listening on port ${app.config.port}`);
+//   });
+// };
+
+
+
+// app.createServer();
