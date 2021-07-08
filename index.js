e22f19d662496e88bdca69f8efa03306f30c80be
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2021-07-09 01:26:52 +0600

    final commit

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
-// Title: Updtime Monitoring Application
+// Title: Project Initial file 
 // Description: A RESTFul API to monitor up or down time of user defined links
 // Author: Mohiuddin Mazumder
 // Date: 19/06/2021
 
 //Dependencies
-const http = require("http");
-const {handleReqRes} = require("./helpers/handleReqRes");
-const environment = require('./helpers/environment');
-const data = require('./lib/data');
-const {sendTwilioSms} = require('./helpers/notifications');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 //app object = module scaffolding
 const app = {};
 
-//Todo remove later
-sendTwilioSms('01934373828', 'Hello world', (err)=>{
-  console.log(`this is the error`, err)
-})
+app.init = () => {
+  // start the server
+  server.init();
+  // start the worker
+  workers.init();
+}
 
+app.init();
 
-
-//handle request response
-app.handleReqRes = handleReqRes;
-
-//create server
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  server.listen(environment.port, () => {
-    console.log(`listening to port ${environment.port}`);
-  });
-};
-
-
-//start the server
-app.createServer();
+module.exports = app;
